36ad0d179a6d478cea1f68eb2ab21b739f24d91c
Johan Collao Vivanco <collao.johan@outlook.com>
2025-01-16 00:54:36 -0300

    Segunda revision , creacion del header About me

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 'use client' 
 import Navbar from "./components/Navbar";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Header/>
     </>
   );
 }
